86ea9d14df721afcb259d377bbeeef8159f3ef4a
chien.truong <qchien.t@gmail.com>
2022-01-10 14:10:27 +0700

    + add KemNFT.sold + set up npm + add hardhat + add openzeppelin + add solidity

---
+++ contracts/KemNFT.sol
@@ -0,0 +1,31 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+//the kem-nft handler
+// inherited from erc721 (ETH) and ownable
+contract KemNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    //the construction is publish once , so dont use this as public
+    constructor() ERC721("KEMNFT", "NFT") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
